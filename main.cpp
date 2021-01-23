f7bd0e1aca72a5c0aa11b0c8292844f7c0e8c3a0
zagav <kompas-udm@mail.ru>
2021-01-23 22:55:05 +0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "lib.h"
+
+#include <spdlog/spdlog.h>
+
+int main()
+{
+  //  auto logger = spdlog::stdout_logger_mt("console");
+  //  logger->info("version {} was started", version());
+
+	int a=0;
+
+    return 0;
+}
